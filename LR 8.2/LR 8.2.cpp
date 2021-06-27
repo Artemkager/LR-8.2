b63c7ea1ed2517f3fa58405aab4dfe3fa2a32789
airex <airex@DESKTOP-PAB22CS>
2021-06-27 20:15:37 +0300

    Добавьте файлы проекта.

---
+++ LR 8.2/LR 8.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void function()
+{
+	cout << "Введите нижний регистр\n";
+	char a; // определяем символьный тип данных a
+	cin >> a;
+	cout << "Вывод верхнего регистра\n" << (char)toupper(a) << endl;;// при помощи функции toupper позволяет заменять буквы нижнего регистра в верхний регистр
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	function(); // вызов функции
+	system("pause");
+	return 0;
+}
